ec18bf5c66a33c2150a48b5654f35b304a588da6
Homero <homero6834@gmail.com>
2024-08-06 17:16:13 -0500

    cors 2

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -6,16 +6,12 @@ import indexRoutes from "./routes/index.routes.js";
 
 
 const app = express();
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://app-funorsal.vercel.app/');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+
 
 app.use(express.json());
 const whiteList = ["http://localhost:4200",
   "https://app-funorsal.vercel.app/",
+  "https://app-funorsal.vercel.app/login",
   "https://app-funorsal-5jcrjy3bj-homero22s-projects.vercel.app/"
 ];
 
