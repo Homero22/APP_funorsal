5c18278d603ad9639851b91b23a83bc93365513f
Homero <homero6834@gmail.com>
2024-08-06 16:33:01 -0500

    C:/Program Files/Git/

--- cliente/src/environments/environment.prod.ts
+++ cliente/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   baseUrl: baseUrl,
   url: 'https://tesis/' + baseUrl,
   urlLogOut: 'https://tesis/logout' + baseUrl,
-  urlApi: 'appfunorsal-production.up.railway.app',
+  urlApi: 'appfunorsal-production.up.railway.app/',
 
 };
